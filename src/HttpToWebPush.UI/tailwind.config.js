d9ccab59a157912236c89718b244e694d5e973e6
patrick <patrick.bachmann+github@outlook.com>
2021-11-19 12:09:41 +0100

    add *.config.js

---
+++ src/HttpToWebPush.UI/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './**/*.html',
+        './**/*.cshtml',
+        './**/*.razor'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
